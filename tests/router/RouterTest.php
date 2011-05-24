a5879c2af095749028c482644dcf2aa4e013c272
Roger Llopart Pla <lumbendil@gmail.com>
2011-05-24 16:58:06 +0200

    Created the skeleton for the router test class.

---
+++ tests/router/RouterTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * RouterTest.php
+ * 
+ * File wich contains the Router tests.
+ *
+ * @author Roger Llopart Pla <lumbendil@gmail.com>
+ * @version 0.1
+ * @package eostis
+ * @subpackage tests
+ */
+
+/**
+ * PHPUnit class to test the router.
+ */
+class RouterTest extends PHPUnit_Framework_TestCase
+{}
