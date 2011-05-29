b1c2d1dd8f968ae2911f6472ef273a6c2454d4f1
Roger Llopart Pla <lumbendil@gmail.com>
2011-05-29 18:11:13 +0200

    [Closes #1]: Made the router pass all it's tests.

--- router/router.php
+++ router/router.php
@@ -38,6 +38,13 @@ class Router implements RouterInterface
 	protected $params = NULL;
 
 	/**
+	 * The routing information.
+	 *
+	 * @var array
+	 */
+	protected $router = NULL;
+
+	/**
 	 * Constructor, wich prepares the router to work with a given set of routes.
 	 *
 	 * @param array $routes The routes, in the following format:<br>
@@ -56,12 +63,64 @@ class Router implements RouterInterface
 	 * 	[...]
 	 * )
 	 * </pre></code>
+	 *
+	 * param_regex must not have the delimiters, and must escape the @ (those
+	 * will be the delimiters of the regex). Also, they must not contain
+	 * capturing sequences (parentheses).
 	 */
 	public function __construct( $routes )
-	{}
+	{
+		$this->routes = $routes;
+	}
 
 	public function parseRequest( \eostis\request\Request $request )
-	{}
+	{
+		foreach( $this->routes as $route_info )
+		{
+			$route_regex = $this->getRouteRegex($route_info['route'],
+				$route_info['params']);
+
+			if( preg_match( $route_regex, $request->getUri(), $matches ) )
+			{
+				$this->controller = $route_info['controller'];
+				$this->action = $route_info['action'];
+				$this->params = array();
+
+				$match_index = 1;
+				foreach( array_keys( $route_info['params'] ) as $key )
+				{
+					$this->params[$key] = $matches[$match_index++];
+				}
+
+				return true;
+			}
+		}
+
+		$this->controller	= NULL;
+		$this->action		= NULL;
+		$this->params		= NULL;
+
+		return false;
+	}
+
+	/**
+	 * Given a route and all it's params, it gets the regex to match.
+	 *
+	 * @param string $route
+	 * @param array $params
+	 *
+	 * @return string
+	 */
+	protected function getRouteRegex( $route, $params )
+	{
+		foreach( $params as $param_name => $param_regex )
+		{
+			$route = str_replace("[[$param_name]]", '(' . $param_regex . ')'
+				, $route);
+		}
+
+		return '@^' . $route . '$@';
+	}
 
 
 	public function getController()
